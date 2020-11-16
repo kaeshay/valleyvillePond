a0e58e89237ffee38e89bbc9e395a84edd7acc83
Kay <kay.liang@outlook.com>
2020-11-16 10:12:46 -0500

    pond assets

---
+++ scripts/draggable.js
@@ -0,0 +1,29 @@
+var positions = JSON.parse(localStorage.positions || "{}");
+$(function () {
+    var d = $("[id=draggable]").attr("id", function (i) {
+        return "draggable_" + i
+    })
+    $.each(positions, function (id, pos) {
+        $("#" + id).css(pos)
+    })
+
+    d.draggable({
+        containment: "#containment-wrapper",
+        scroll: false,
+        stop: function (event, ui) {
+            positions[this.id] = ui.position
+            localStorage.positions = JSON.stringify(positions)
+        }
+    });
+            
+});
+
+// inventory toggle
+// $('.charlie').dblclick(function() {
+//     $('#charlie-inventory').toggle();
+
+
+$(function() {
+    $( ".draggable" ).draggable();
+  });
+
